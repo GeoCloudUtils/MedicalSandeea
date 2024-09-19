ad6e47056452a2a738c442d1435b70313264b8fb
Gheorghe <gheorghe@edituraedu.ro>
2024-09-19 14:37:21 +0300

    Work in progress

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-const $body = $("body");
-const $header = $(".page-header");
-const $navCollapse = $(".navbar-collapse");
-const scrollClass = "scroll";
+$(document).ready(function () {
+  $(".show-more-btn").click(function () {
+    // Get the target container ID from the data attribute
+    var targetContainer = $(this).data("target");
 
-$(window).on("scroll", () => {
-  if (this.matchMedia("(min-width: 992px)").matches) {
-    const scrollY = $(this).scrollTop();
-    scrollY > 0 ? $body.addClass(scrollClass) : $body.removeClass(scrollClass);
-  } else {
-    $body.removeClass(scrollClass);
-  }
-});
+    // Show all hidden portfolio items within the target container
+    $(targetContainer + " .portfolio-item.d-none").removeClass("d-none");
 
-$(".page-header .nav-link, .navbar-brand").on("click", function (e) {
-  e.preventDefault();
-  const href = $(this).attr("href");
-  $("html, body").animate(
-    {
-      scrollTop: $(href).offset().top - 71,
-    },
-    600
-  );
+    // Optionally hide the button after clicking
+    $(this).hide();
+  });
 });
